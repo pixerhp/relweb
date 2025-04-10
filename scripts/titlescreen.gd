2c1f8a46cbbbe752b778af78db4629f15486f8a7
Pixer Pinecone <99830732+pixerhp@users.noreply.github.com>
2025-04-10 16:29:52 -0400

    Implement checks for title/version text setting

--- scripts/titlescreen.gd
+++ scripts/titlescreen.gd
@@ -1,6 +1,9 @@
 extends Node
 
 func _ready():
-	%TSProgramName.text = "[center]" + Globals.PROGRAM_NAME.to_upper() + "[/center]"
-	%TSVersionText.text = "version " + Globals.PROGRAM_VERSION
+	if not Globals.PROGRAM_NAME.is_empty():
+		%TSProgramName.text = "[center]" + Globals.PROGRAM_NAME.to_upper() + "[/center]"
+	if not Globals.PROGRAM_VERSION.is_empty():
+		%TSVersionText.text = "version " + Globals.PROGRAM_VERSION
+	
 	return
