1bb996cb834bb3c059cfc15dad961ad802c94ea5
Pixer Pinecone <99830732+pixerhp@users.noreply.github.com>
2025-04-11 14:58:34 -0400

    Implement quit button functionality

--- scripts/titlescreen.gd
+++ scripts/titlescreen.gd
@@ -12,3 +12,7 @@ func _ready():
 func _on_button_file_loc_pressed():
 	OS.shell_show_in_file_manager(ProjectSettings.globalize_path("user://"))
 	return
+
+func _on_button_quit_pressed():
+	get_tree().quit()
+	return
